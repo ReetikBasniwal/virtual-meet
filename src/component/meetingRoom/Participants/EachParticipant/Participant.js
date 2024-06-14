968b5465561362b798ddfe7f4ca534faf2787bf2
Reetik Basniwal <reetikbasniwal@gmail.com>
2024-06-14 23:22:09 +0530

    add media

--- src/component/meetingRoom/Participants/EachParticipant/Participant.js
+++ src/component/meetingRoom/Participants/EachParticipant/Participant.js
@@ -1,14 +1,38 @@
 import { BsFillMicMuteFill } from 'react-icons/bs';
 import { Card } from '../../../Shared/Card/CardComponent';
 import './Participant.css';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
 
 export const Participant = ({ participantData }) => {
-    
+    const videoRef = useRef();
+
+    const remoteStream = new MediaStream();
+
+    const userStream = useSelector(state => state.roomReducer.mainStream)
+
+    useEffect(() => {
+        if(participantData.peerConnection) {
+            participantData.peerConnection.ontrack = (event) => {
+                event.streams[0].getTracks().forEach(track => {
+                    remoteStream.addTrack(track);
+                })
+
+                videoRef.current.srcObject = remoteStream;
+            }
+        }
+    },[participantData.peerConnection])
+
+    useEffect(() => {
+        if(userStream && participantData.currentUser) {
+            videoRef.current.srcObject = userStream;
+        }
+    },[participantData.currentUser, userStream])
+
     return (
         <div className='participant'>
             <Card>
-                <video className="video" autoPlay playsInline></video>
+                <video ref={videoRef} className="video" autoPlay playsInline></video>
                 <div className='muted'><BsFillMicMuteFill /></div>
                 <div style={{backgroundColor: participantData.avatarColor }} className="avatar">{participantData.userName[0]}</div>
                 <div className="name">
