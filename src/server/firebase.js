f94b762e5304d05865b22e27344222ee91ec26ac
Reetik Basniwal <reetikbasniwal@gmail.com>
2024-06-26 20:47:01 +0530

    remove extra things

--- src/server/firebase.js
+++ src/server/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAXujsZDcE-I_h4S6u8bu7cyqrWiqaaSLQ",
-    authDomain: "virtual-meet-88e3e.firebaseapp.com",
-    databaseURL: "https://virtual-meet-88e3e-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "virtual-meet-88e3e",
-    storageBucket: "virtual-meet-88e3e.appspot.com",
-    messagingSenderId: "840596868158",
-    appId: "1:840596868158:web:114082299adc9a2e8dd56f"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 }
 
 const app = initializeApp(firebaseConfig);
