586f9c8ba89b44d7934688698aeea0f4175c1a37
Reetik Basniwal <reetikbasniwal@gmail.com>
2024-07-01 21:56:00 +0530

    remove extra console

--- src/component/meetingRoom/MainScreen/MeetingFooter/MeetingFooter.js
+++ src/component/meetingRoom/MainScreen/MeetingFooter/MeetingFooter.js
@@ -125,7 +125,7 @@ export default function MeetingFooter() {
         navigate('/');
         dispatch(roomActions.setisRoomActive(true));
     }
-console.log(userPrefernce, "userPrefernce")
+    
     return (
         <div className="h-full w-full flex items-center justify-center">
             <div className={`${userPrefernce.audio ? 'meetingIcons' : 'redMeetingIcons'}`} onClick={() => togglePreference("audio", !userPrefernce.audio)}>
